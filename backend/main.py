cdfa8d1a53b01c45c757dd5d3559ea12a4e6e1fb
angge <166150697+anggevictoria@users.noreply.github.com>
2024-08-23 09:20:18 +0800

    com4

--- backend/main.py
+++ backend/main.py
@@ -2,6 +2,6 @@ from local import HomeControl
 
 if __name__ == "__main__":
     # Initialize HomeControl with your specific COM port and baud rate
-    home_control = HomeControl('COM3', 9600)
+    home_control = HomeControl('COM4', 9600)
     # Run the main loop
     home_control.main_loop()
